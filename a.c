30004dfb3c3cdf4e32a9059dd2cac7e72748a683
Alanscut <wp_scut@163.com>
2020-05-20 15:21:17 +0800

    initiate a.c

---
+++ a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("===================start===================\n");
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int d = a + b + c;
+    printf("d = %d\n");
+    printf("===================end===================\n");
+
+    return 0;    
+}
